1ebdd32e09a0fe2c4eade29b7df82a4d042fe30c
MD Hussain <>
2021-03-13 00:08:35 +0530

    Update index

--- src/index.js
+++ src/index.js
@@ -1,20 +1,23 @@
-import React from "react"
-import ReactDOM from "react-dom"
-import DigitalClockComponent from "./DigitalClock"
+import React from "react";
+import ReactDOM from "react-dom";
+import DigitalClockComponent from "./DigitalClock";
 class DigitalClock extends React.Component {
-    state = {currentTime :  new Date().toLocaleTimeString()}
-    componentDidMount(){
-        setInterval(()=>{
-            this.setState({currentTime:new Date().toLocaleTimeString()})
-        },1000)
-    }
-    render(){
-        return(
-            <div>
-                <DigitalClockComponent currentTimeProp={this.state.currentTime} currentDay ={new Date().toLocaleDateString()}/>
-            </div>
-        )
-    }
+  state = { currentTime: new Date().toLocaleTimeString() };
+  componentDidMount() {
+    setInterval(() => {
+      this.setState({ currentTime: new Date().toLocaleTimeString() });
+    }, 1000);
+  }
+  render() {
+    return (
+      <div>
+        <DigitalClockComponent
+          currentTimeProp={this.state.currentTime}
+          currentDay={new Date().toLocaleDateString()}
+        />
+      </div>
+    );
+  }
 }
 
-ReactDOM.render(<DigitalClock/>,document.getElementById('root'))
+ReactDOM.render(<DigitalClock />, document.getElementById("root"));
